237fc773f66ac75fd2e0abe9825990f904b15fa4
quangkhai78py <quangkhai78py@gmail.com>
2024-09-01 10:36:31 +0700

    Update code

--- src/module/passkey/passkey.service.ts
+++ src/module/passkey/passkey.service.ts
@@ -25,9 +25,9 @@ import type {
 } from '@simplewebauthn/types';
 import { User } from '../user/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
-import { QueryRunner, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Passkey } from './entities/passkey.entity';
-import { runQuery } from 'src/share/util/helper.util';
+import { CommonLogger } from 'src/share/common/logger/common.logger';
 
 const rpID = process.env.RPID;
 
@@ -40,132 +40,153 @@ export class PasskeyService {
 	) {}
 
 	public async generateRegistrationOption(user: User): Promise<PublicKeyCredentialCreationOptionsJSON> {
-		const options = await generateRegistrationOptions({
-			rpName: 'Nillion Passkey Demo',
-			rpID: rpID,
-			userID: Buffer.from(user.id.toString()),
-			userName: user.email,
-			timeout: 60000,
-			attestationType: 'direct',
-			excludeCredentials: [],
-			authenticatorSelection: {
-				residentKey: 'preferred',
-			},
-			supportedAlgorithmIDs: [-8],
-		});
+		try {
+			const options = await generateRegistrationOptions({
+				rpName: 'Nillion Passkey Demo',
+				rpID: rpID,
+				userID: Buffer.from(user.id.toString()),
+				userName: user.email,
+				timeout: 60000,
+				attestationType: 'direct',
+				excludeCredentials: [],
+				authenticatorSelection: {
+					residentKey: 'preferred',
+				},
+				supportedAlgorithmIDs: [-8],
+			});
 
-		await this.cacheManager.set(`REGISTRATION-${user.id.toString()}`, options.challenge);
-		return options;
+			await this.cacheManager.set(`REGISTRATION-${user.id.toString()}`, options.challenge);
+			return options;
+		} catch (error) {
+			CommonLogger.log(`${new Date().toDateString()}_ERRORS_POST_GENERATE_REGISTRATION_PASSKEY_SERVICE_`, error);
+			throw error;
+		}
 	}
 
 	public async verifyRegistration(user: User, credential: RegistrationResponseJSON): Promise<boolean> {
-		const expectedChallenge = await this.cacheManager.get<string>(`REGISTRATION-${user.id.toString()}`);
-
-		let verification: VerifiedRegistrationResponse;
 		try {
-			const opts: VerifyRegistrationResponseOpts = {
-				response: credential,
-				expectedChallenge: `${expectedChallenge}`,
-				expectedOrigin: process.env.ORIGIN_URL,
-				expectedRPID: rpID,
-				requireUserVerification: false,
-			};
-			verification = await verifyRegistrationResponse(opts);
-		} catch (error) {
-			const _error = error as Error;
-			console.error('_error', _error);
-			return false;
-		}
+			const expectedChallenge = await this.cacheManager.get<string>(`REGISTRATION-${user.id.toString()}`);
 
-		const { verified, registrationInfo } = verification;
+			let verification: VerifiedRegistrationResponse;
+			try {
+				const opts: VerifyRegistrationResponseOpts = {
+					response: credential,
+					expectedChallenge: `${expectedChallenge}`,
+					expectedOrigin: process.env.ORIGIN_URL,
+					expectedRPID: rpID,
+					requireUserVerification: false,
+				};
+				verification = await verifyRegistrationResponse(opts);
+			} catch (error) {
+				const _error = error as Error;
+				console.error('_error', _error);
+				return false;
+			}
 
-		if (verified && registrationInfo) {
-			const { credentialPublicKey, credentialID, counter } = registrationInfo;
-			const existingDevice = await this.passkeyModel.findOne({
-				where: { internal_user_id: user.id, cred_id: credentialID },
-			});
-			if (!existingDevice) {
-				const newDevice: Passkey = this.passkeyModel.create({
-					cred_id: credentialID,
-					cred_public_key: credentialPublicKey,
-					internal_user_id: user.id,
-					counter: counter,
-					transports: credential.response.transports.join('|'),
-					webauthn_user_id: '',
-					backup_eligible: false,
-					backup_status: false,
+			const { verified, registrationInfo } = verification;
+
+			if (verified && registrationInfo) {
+				const { credentialPublicKey, credentialID, counter } = registrationInfo;
+				const existingDevice = await this.passkeyModel.findOne({
+					where: { internal_user_id: user.id, cred_id: credentialID },
 				});
 
-				await this.passkeyModel.save(newDevice);
+				if (!existingDevice) {
+					const newDevice: Passkey = this.passkeyModel.create({
+						cred_id: credentialID,
+						cred_public_key: Buffer.from(credentialPublicKey),
+						internal_user_id: user.id,
+						counter: counter,
+						transports: credential.response.transports.join('|'),
+						webauthn_user_id: '',
+						backup_eligible: false,
+						backup_status: false,
+					});
+
+					await this.passkeyModel.save(newDevice);
+				}
 			}
+
+			await this.cacheManager.del(`REGISTRATION-${user.id.toString()}`);
+
+			return verified;
+		} catch (error) {
+			CommonLogger.log(`${new Date().toDateString()}_ERRORS_POST_VERIFY_REGISTRATION_PASSKEY_SERVICE_`, error);
+			throw error;
 		}
-
-		await this.cacheManager.del(`REGISTRATION-${user.id.toString()}`);
-
-		return verified;
 	}
 
 	public async generateAuthenticationOption(user: User): Promise<PublicKeyCredentialRequestOptionsJSON> {
-		const devices = await this.passkeyModel.find({
-			where: { internal_user_id: user.id },
-		});
-		if (!devices || devices.length === 0) throw Error('Not found passkey device');
+		try {
+			const devices = await this.passkeyModel.find({
+				where: { internal_user_id: user.id },
+			});
+			if (!devices || devices.length === 0) throw Error('Not found passkey device');
 
-		const opts: GenerateAuthenticationOptionsOpts = {
-			timeout: 60000,
-			allowCredentials: devices.map((dev) => ({
-				id: dev.cred_id,
-				type: 'public-key',
-				transports: dev.transports.split('|') as AuthenticatorTransportFuture[],
-			})),
-			userVerification: 'preferred',
-			rpID,
-		};
+			const opts: GenerateAuthenticationOptionsOpts = {
+				timeout: 60000,
+				allowCredentials: devices.map((dev) => ({
+					id: dev.cred_id,
+					type: 'public-key',
+					transports: dev.transports.split('|') as AuthenticatorTransportFuture[],
+				})),
+				userVerification: 'preferred',
+				rpID,
+			};
 
-		const options = await generateAuthenticationOptions(opts);
-		await this.cacheManager.set(`AUTHENTICATION-${user.id.toString()}`, options.challenge);
+			const options = await generateAuthenticationOptions(opts);
+			await this.cacheManager.set(`AUTHENTICATION-${user.id.toString()}`, options.challenge);
 
-		return options;
+			return options;
+		} catch (error) {
+			CommonLogger.log(`${new Date().toDateString()}_ERRORS_POST_GENERATE_AUTHENTICATION_PASSKEY_SERVICE_`, error);
+			throw error;
+		}
 	}
 
 	public async verifyAuthentication(user: User, credential: AuthenticationResponseJSON): Promise<boolean> {
-		const expectedChallenge = await this.cacheManager.get<string>(`AUTHENTICATION-${user.id.toString()}`);
-
-		const device = await this.passkeyModel.findOne({ where: { internal_user_id: user.id, cred_id: credential.id } });
-		if (!device) return false;
-
-		const dbAuthenticator: AuthenticatorDevice = {
-			credentialID: device.cred_id,
-			credentialPublicKey: device.cred_public_key,
-			counter: device.counter,
-			transports: device.transports.split('|') as AuthenticatorTransportFuture[],
-		};
-
-		let verification: VerifiedAuthenticationResponse;
 		try {
-			const opts: VerifyAuthenticationResponseOpts = {
-				response: credential,
-				expectedChallenge,
-				expectedOrigin: process.env.ORIGIN_URL,
-				expectedRPID: rpID,
-				authenticator: dbAuthenticator,
-				requireUserVerification: false,
+			const expectedChallenge = await this.cacheManager.get<string>(`AUTHENTICATION-${user.id.toString()}`);
+
+			const device = await this.passkeyModel.findOne({ where: { internal_user_id: user.id, cred_id: credential.id } });
+			if (!device) return false;
+
+			const dbAuthenticator: AuthenticatorDevice = {
+				credentialID: device.cred_id,
+				credentialPublicKey: device.cred_public_key,
+				counter: device.counter,
+				transports: device.transports.split('|') as AuthenticatorTransportFuture[],
 			};
-			verification = await verifyAuthenticationResponse(opts);
+
+			let verification: VerifiedAuthenticationResponse;
+			try {
+				const opts: VerifyAuthenticationResponseOpts = {
+					response: credential,
+					expectedChallenge,
+					expectedOrigin: process.env.ORIGIN_URL,
+					expectedRPID: rpID,
+					authenticator: dbAuthenticator,
+					requireUserVerification: false,
+				};
+				verification = await verifyAuthenticationResponse(opts);
+			} catch (error) {
+				const _error = error as Error;
+				console.error(_error);
+				return false;
+			}
+
+			const { verified, authenticationInfo } = verification;
+			if (verified) {
+				device.counter = authenticationInfo.newCounter;
+				await this.passkeyModel.save(device);
+			}
+
+			await this.cacheManager.del(`AUTHENTICATION-${user.id.toString()}`);
+
+			return verified;
 		} catch (error) {
-			const _error = error as Error;
-			console.error(_error);
-			return false;
+			CommonLogger.log(`${new Date().toDateString()}_ERRORS_POST_VERIFY_AUTHENTICATION_PASSKEY_SERVICE_`, error);
+			throw error;
 		}
-
-		const { verified, authenticationInfo } = verification;
-		if (verified) {
-			device.counter = authenticationInfo.newCounter;
-			await this.passkeyModel.save(device);
-		}
-
-		await this.cacheManager.del(`AUTHENTICATION-${user.id.toString()}`);
-
-		return verified;
 	}
 }
